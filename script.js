4c8710a007c5cd00ca2c4d91bd4247c82d83c061
bastiscnr <schachner.sebastian@hak-liezen.at>
2023-05-02 09:53:36 +0200

    2023-05-02

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
     $(document).ready(function(){
+
+        if ('serviceWorker' in navigator) {
+
+            // We want to register the service worker file with the browser
+            
+            navigator.serviceWorker.register('sw.js', {
+            
+            // Just the file scope; since this is in the main directory, we'll leave it blank. This is kind of a finicky option, FYI.
+            
+            scope: ''
+            
+            }).then(function(reg) {
+            
+            // registration worked, hurray!
+            
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+            
+            
+            
+            
+            if (reg.installing) {
+            
+            console.log('Service worker installing');
+            
+            } else if (reg.waiting) {
+            
+            console.log('Service worker installed');
+            
+            } else if (reg.active) {
+            
+            console.log('Service worker active');
+            
+            }
+            
+            }).catch(function(error) {
+            
+            // registration failed. No worries, just make sure HTTPS is enabled and you're calling the SW correctly.
+            
+            console.log('Registration failed with ' + error);
+            
+            });
+            
+            }
+
         $("#showTemperature").click(function(){
             $("#startMenu").fadeOut();
             $("#temperature").fadeIn();
